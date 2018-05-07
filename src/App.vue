1f854c75b5e2bf7b2c3acfc1e458b4e4aff8312b
lfhwnqe <464385492@qq.com>
2018-05-07 22:37:45 +0800

    postcss兼容

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,47 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
-</template>
+<template >
+  <div id = "app" >
+    <img src = "./assets/logo.png" >
+    <ul >
+      <li class = "first" >1</li >
+      <li class = "second" >2</li >
+      <li class = "third" >3</li >
+    </ul >
+    <router-view />
+  </div >
+</template >
 
-<script>
-export default {
-  name: 'App'
-}
-</script>
+<script >
+  export default {
+    name: 'App'
+  }
+</script >
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style scoped >
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  ul {
+    height: 1rem;
+  }
+
+  .first {
+    width: 3rem;
+    background: red;
+  }
+
+  .second {
+    width: 6rem;
+    background: blue;
+  }
+
+  .third {
+    width: 9rem;
+    background: yellow;
+  }
+</style >
